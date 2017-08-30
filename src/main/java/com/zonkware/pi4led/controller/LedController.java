d8494c388008b4de54b20122f76f502cc5282047
ccm026 <ccm026@gmail.com>
2017-08-30 20:23:21 +0200

    may be again hopefully

--- src/main/java/com/zonkware/pi4led/controller/LedController.java
+++ src/main/java/com/zonkware/pi4led/controller/LedController.java
@@ -1,5 +1,6 @@
 package com.zonkware.pi4led.controller;
 
+
 import com.pi4j.io.gpio.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,19 +12,18 @@ public class LedController {
 
     @RequestMapping("/")
     public String greeting() {
-        return "Hello world";
+        return "Hello world again";
     }
+
     @RequestMapping("/light")
     public String light() {
-        if (pin == null) {
+        if (pin == null){
             GpioController gpio = GpioFactory.getInstance();
             pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02, "MyLED", PinState.LOW);
         }
-
-        pin.toggle();
+    pin.toggle();
 
         return "OK";
-
     }
 
 }
